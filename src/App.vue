c867c13a5f93564e50216b79bc9d0486b1ecc3e5
HANACYGM <mailcygm@gmail.com>
2020-11-09 16:18:22 +0800

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <HelloWorld msg="Hello1 Vue 3.0 + Vite" />
+	<HelloWorld msg="Hello3 Vue 3.0 + Vite" />
 </template>
 
 <script>
-import { createApp } from "vue";
+	import { createApp } from "vue";
+import { Col, Row } from 'vant';
 import HelloWorld from "./components/HelloWorld.vue";
+
+const app = createApp();
+app.use(Col);
+app.use(Row);
 
 export default {
   name: "App",
